dd57a1104021850895472ce53f2c720b0182341c
sooloth <pangnuar@gmail.com>
2021-11-01 13:21:46 +0700

    first commit

---
+++ blog/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI,status,Response,HTTPException
+from fastapi.params import Depends
+from . import schamas,models
+from .database import SessionLocal, engine
+from sqlalchemy.orm import Session
+
+
+app = FastAPI()
+
+
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schamas.Blog, db:Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title,body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+#delete blog
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db:Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+
+    return 'done'
+
+#update
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id,request:schamas.Blog, db:Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f"Blog with id {id} not found")
+    blog.update({'title': request.title, 'body': request.body})
+    db.commit()
+    return 'updated'
+
+@app.get('/blog')#get blog from database
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return  blogs
+
+@app.get('/blog/{id}',status_code=200)#filter
+def show(id, response:Response,db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Blog with the id {id} is not available')
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail':f'Blog with the id {id} is not available'}
+    return blog
--- main.py
+++ main.py
@@ -1,8 +1,40 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/blog')#query parameter == ?limit=50&
 
-def index():
-    return 'Welcome'
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
+    #only get 10 published
+    if published:
+        return {'data': f'{limit}blog list '}
+    else:
+         return {'data': f'{limit}blog from database'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id 
+    return {'data': id}
+
+
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blogs with id = id
+    return {'data': {'1','2'}}
+
+class Blog(BaseModel):
+    title:str
+    body:str 
+    published: Optional[bool]
+
+@app.post('/blog')
+def create_blog(request:Blog):
+    return {'data': f"Blog is creted with title as {request.title}"}
